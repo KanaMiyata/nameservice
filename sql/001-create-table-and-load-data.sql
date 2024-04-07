41caaab872ce8b6ddc326fe8c2f7eb6879f069bb
kana <chocolate.bar.favorite@gmail.com>
2024-04-08 00:22:08 +0900

    exceptionの実装

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -3,11 +3,10 @@ DROP TABLE IF EXISTS names;
 CREATE TABLE names (
  id int unsigned AUTO_INCREMENT,
  name VARCHAR(20) NOT NULL,
+    age INT,
  PRIMARY KEY(id)
 );
 
-INSERT INTO names (name,age) VALUES ('Kana');
-INSERT INTO names (name,age) VALUES ('Naoko');
-INSERT INTO names (name,age) VALUES ('Cisco');
-
-
+INSERT INTO names (name, age) VALUES ('Kana', 32);
+INSERT INTO names (name, age) VALUES ('Naoko', 30);
+INSERT INTO names (name, age) VALUES ('Cisco', 31);
--- src/main/java/com/example/nameservice/NameController.java
+++ src/main/java/com/example/nameservice/NameController.java
@@ -1,12 +1,9 @@
 package com.example.nameservice;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -25,17 +22,7 @@ public class NameController {
         return nameMapper.findByNameStartingWith(startsWith);
     }
 }
-@ExceptionHandler(value = NameNotFoundException.class)
-public ResponseEntity<Map<String, String>> handleUserNotFoundException(
-        NotFoundException e, HttpServletRequest request) {
-    Map<String, String> body = Map.of(
-            "timestamp", ZonedDateTime.now().toString(),
-            "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
-            "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
-            "message", e.getMessage(),
-            "path", request.getRequestURI());
-    return new ResponseEntity(body, HttpStatus.NOT_FOUND);
-}
+
 
 
 
